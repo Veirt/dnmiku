3b21ad027e7f319dc9c514db776c397e0fe813bc
ExLog <55097092+ExLog@users.noreply.github.com>
2020-12-03 23:06:18 +0800

    Routes get and post

--- routes/pages.js
+++ routes/pages.js
@@ -1,20 +1,30 @@
 const express = require('express');
+const User = require('../core/user')
 const router = express.Router()
+
+const user = new User();
+
 
 router.get('/', (req, res, next) => {
     res.render('index')
 })
-
 router.get('/login', (req, res, next) => {
     res.render('login')
 })
-
 router.get('/register', (req, res, next) => {
     res.render('register')
 })
 
+
 router.post('/login', (req, res, next) => {
-    res.json(req.body)
+    user.login(req.body.id, req.body.password, function (result) {
+        res.json(req.body)
+        if (result) {
+            res.send('Logged in as : ' + result.username)
+        } else {
+            res.send('Username or password is incorrect')
+        }
+    })
 })
 
 router.post('/register', (req, res, next) => {
