3824369a8abb3e96025c6088bff8eb0113af4aff
ExLog <55097092+ExLog@users.noreply.github.com>
2020-12-22 03:20:05 +0800

    Connect to all database

--- core/db.js
+++ core/db.js
@@ -3,12 +3,13 @@ const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_HOST,
-    database: process.env.DB,
     options: {
         enableArithAbort: true,
         encrypt: true
     }
 }
+
+
 const poolPromise = new sql.ConnectionPool(config, (err) => {
     if (err) {
         console.log(err)
