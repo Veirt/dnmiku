80b2bbddc7614856d494c4f5df0e6bc1746925c3
ExLog <55097092+ExLog@users.noreply.github.com>
2020-12-27 00:44:57 +0800

    Fixed query (2)

--- query/addCashOnlineProcedure.sql
+++ query/addCashOnlineProcedure.sql
@@ -1,6 +1,7 @@
 CREATE PROCEDURE [dbo].[AddCash]
 @IntCashAmount nvarchar (10)
 AS
+
 UPDATE Accounts
 SET cash += @IntCashAmount
-WHERE EXISTS (SELECT AccountDBID FROM DNMembership.dbo.DNAuth WHERE DNMembership.DNAuth.AccountDBID = Accounts.AccountID AND DNMembership.DNAuth.CertifyingStep = 2);
+WHERE EXISTS (SELECT AccountDBID FROM DNAuth WHERE DNAuth.AccountDBID = Accounts.AccountID AND DNAuth.CertifyingStep = 2); 
