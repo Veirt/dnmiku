e104c2f07ba39b1e0fd337ddbd94ae6f47ad98c8
ExLog <55097092+ExLog@users.noreply.github.com>
2021-01-01 19:09:13 +0800

    Fixed wrong column name

--- routes/routesPost.ts
+++ routes/routesPost.ts
@@ -76,7 +76,7 @@ router.post('/register', urlencodedParser, [
                     .input("id", sql.NVarChar(50), req.body.id)
                     .input("password", sql.VarChar(12), req.body.password)
                     .input("email", sql.VarChar(50), req.body.email)
-                    .query("INSERT INTO DNMembership.dbo.Accounts (AccountName, AccountLevelCode, CharacterCreateLimit, CharacterMaxCount, RegisterDate, PublisherCode, Passphrase, mail) VALUES (@id, 99, 4, 8, GETDATE(), 0, CONVERT(BINARY(20),HashBytes('MD5',@password),2), @email)")
+                    .query("INSERT INTO DNMembership.dbo.Accounts (AccountName, AccountLevelCode, CharacterCreateLimit, CharacterMaxCount, RegisterDate, PublisherCode, Passphrase, Email) VALUES (@id, 99, 4, 8, GETDATE(), 0, CONVERT(BINARY(20),HashBytes('MD5',@password),2), @email)")
 
                 req.session.message = 'Registered succesfully'
                 res.redirect('/login')
