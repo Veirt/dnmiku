b9e768e14e627d711898e9906f9945b8acb3c272
ExLog <55097092+ExLog@users.noreply.github.com>
2021-01-04 00:16:47 +0800

    Added https and ssl certificate

--- app.ts
+++ app.ts
@@ -1,53 +1,61 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import helmet from 'helmet'
-import path from 'path'
-import session from 'express-session'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import path from "path";
+import session from "express-session";
+import cors from "cors";
+import fs from "fs";
+import http from "http";
+import https from "https";
 
+// https
+const privateKey = fs.readFileSync("ssl/dnmiku.key", "utf8");
+const certificate = fs.readFileSync("ssl/dnmiku.crt", "utf8");
+const credentials = { key: privateKey, cert: certificate };
 // Express Server
 const app = express();
 
 // .env
 dotenv.config({
-    path: './.env'
+  path: "./.env",
 });
 
 // Express Session
-app.use(session({
-    secret: process.env.SECRET_SESSION ?? '',
+app.use(
+  session({
+    proxy: process.env.NODE_ENV === "PROD" ? true : false,
+    secret: process.env.SECRET_SESSION ?? "",
     resave: false,
     saveUninitialized: false,
     cookie: {
-        httpOnly: true,
-        maxAge: 30 * 24 * 60 * 60 * 1000
-    }
-}));
+      secure: true,
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+  })
+);
 
-// Helmet
+console.log(process.env.NODE_ENV === "DEV" ? false : true);
+// Helmet and cors
 app.use(helmet());
-
-// Cors
 app.use(cors());
 
 // Views
-const publicDirectory = path.join(__dirname, 'public');
+const publicDirectory = path.join(__dirname, "public");
 app.use(express.static(publicDirectory));
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 // Routes
-app.use('/', require('./routes/routesGet'));
-app.use('/', require('./routes/routesPost'));
-app.use('/', require('./routes/routesDashboard'));
+app.use("/", require("./routes/routesGet"));
+app.use("/", require("./routes/routesPost"));
+app.use("/", require("./routes/routesDashboard"));
+app.use((req, res) => res.status(404).render("error404"));
+app.use((req, res) => res.status(403).render("error403"));
 
-// Not found routes
-app.use((req,res) => {
-    res.status(404).render('error404');
-});
+// Start server
 
-// Forbidden routes
-app.use((req,res) => {
-    res.status(403).render('error403');
-});
-// Start server 
-app.listen(1111, () => console.log("http://localhost:1111"));
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(1111, () => console.log("HTTP Server listen on port 1111"));
+httpsServer.listen(2222, () => console.log("HTTP Server listen on port 2222"));
