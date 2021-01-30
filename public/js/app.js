ccf4b146d223fc771485374080f9d84b7591ae71
ExLog <55097092+ExLog@users.noreply.github.com>
2021-01-30 23:41:19 +0800

    Added game & village server status

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,16 @@
-function update(){$("#datetime").html(moment().format("h:mm:ss A"))}setInterval(update,1e3);
+fetch("/api/status")
+  .then((res) => res.json())
+  .then((res) => {
+    var game = document.getElementById("gameserver");
+    var village = document.getElementById("villageserver");
+    game.innerHTML = res.GameStatus;
+    if (res.GameStatus === "Online") {
+    }
+    res.GameStatus === "Online"
+      ? game.classList.add("text-green-500")
+      : game.classList.add("text-red-500");
+    village.innerHTML = res.VillageStatus;
+    res.VillageStatus === "Online"
+      ? village.classList.add("text-green-500")
+      : village.classList.add("text-red-500");
+  });
