c8af288631a02eba98357bbbf105813b79543c03
ExLog <55097092+ExLog@users.noreply.github.com>
2021-02-02 10:34:05 +0800

    Remove unused if statement

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,14 @@
 fetch("/api/status")
-  .then((res) => res.json())
-  .then((res) => {
-    var game = document.getElementById("gameserver");
-    var village = document.getElementById("villageserver");
-    game.innerHTML = res.GameStatus;
-    if (res.GameStatus === "Online") {
-    }
-    res.GameStatus === "Online"
-      ? game.classList.add("text-green-500")
-      : game.classList.add("text-red-500");
-    village.innerHTML = res.VillageStatus;
-    res.VillageStatus === "Online"
-      ? village.classList.add("text-green-500")
-      : village.classList.add("text-red-500");
-  });
+	.then((res) => res.json())
+	.then((res) => {
+		var game = document.getElementById("gameserver");
+		var village = document.getElementById("villageserver");
+		game.innerHTML = res.GameStatus;
+		res.GameStatus === "Online"
+			? game.classList.add("text-green-500")
+			: game.classList.add("text-red-500");
+		village.innerHTML = res.VillageStatus;
+		res.VillageStatus === "Online"
+			? village.classList.add("text-green-500")
+			: village.classList.add("text-red-500");
+	});
