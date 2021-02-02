27d069350365a9e7972309c3b237952982446fbe
ExLog <55097092+ExLog@users.noreply.github.com>
2021-02-03 00:40:08 +0800

    Fetch and merge index

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 fetch("/api/status")
-	.then((res) => res.json())
-	.then((res) => {
+	.then(function (res) {
+		return res.json();
+	})
+	.then(function (res) {
 		var game = document.getElementById("gameserver");
 		var village = document.getElementById("villageserver");
 		game.innerHTML = res.GameStatus;
@@ -11,4 +13,16 @@ fetch("/api/status")
 		res.VillageStatus === "Online"
 			? village.classList.add("text-green-500")
 			: village.classList.add("text-red-500");
+	});
+fetch("api/players")
+	.then(function (res) {
+		return res.json();
+	})
+	.then(function (res) {
+		var accounts = document.getElementById("accounts");
+		var characters = document.getElementById("characters");
+		var online = document.getElementById("online");
+		accounts.innerHTML = res.nowTotalAccount;
+		characters.innerHTML = res.nowTotalCharacter;
+		online.innerHTML = res.nowOnline;
 	});
--- src/routes/routesGet.ts
+++ src/routes/routesGet.ts
@@ -3,50 +3,19 @@ import express from "express";
 const router = express.Router();
 
 // Database
-const db = require("../core/db");
 
 router.get("/", async (req, res) => {
-	try {
-		let getOnlinePlayer = await db.poolPromise
-			.request()
-			.query(
-				"SELECT COUNT(CertifyingStep) AS OnlinePlayer FROM DNMembership.dbo.DNAuth WHERE CertifyingStep = 2"
-			);
-		let getTotalAccount = await db.poolPromise
-			.request()
-			.query(
-				"SELECT COUNT(AccountID) AS TotalAccount FROM DNMembership.dbo.Accounts"
-			);
-		let getTotalCharacter = await db.poolPromise
-			.request()
-			.query(
-				"SELECT COUNT(CharacterID) AS TotalCharacter FROM DNMembership.dbo.Characters"
-			);
-
-		let nowOnline = getOnlinePlayer.recordset[0].OnlinePlayer;
-		let nowTotalAccount = getTotalAccount.recordset[0].TotalAccount;
-		let nowTotalCharacter = getTotalCharacter.recordset[0].TotalCharacter;
-
-		// Check if the session is exist
-		let user = req.session.user;
-		if (user) {
-			res.status(200).render("index_dash", {
-				opp: req.session.opp,
-				nowOnline: nowOnline,
-				nowTotalAccount: nowTotalAccount,
-				nowTotalCharacter: nowTotalCharacter,
-			});
-			return;
-		}
-
+	// Check if the session is exist
+	let user = req.session.user;
+	if (user) {
 		res.status(200).render("index", {
-			nowOnline: nowOnline,
-			nowTotalAccount: nowTotalAccount,
-			nowTotalCharacter: nowTotalCharacter,
+			opp: req.session.opp,
+			loggedInStatus: true,
 		});
-	} catch (err) {
-		console.log(`Unexpected error : ${err}`);
+		return;
 	}
+
+	res.status(200).render("index");
 });
 
 router.get("/download", (req, res) => {
