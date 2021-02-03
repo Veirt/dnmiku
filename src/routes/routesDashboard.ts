1445603a4e14693e4c45c1642cf1574386320550
ExLog <55097092+ExLog@users.noreply.github.com>
2021-02-03 21:47:34 +0800

    Remove body-parser

--- src/routes/routesDashboard.ts
+++ src/routes/routesDashboard.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 import sql from "mssql";
 import schedule from "node-schedule";
@@ -9,119 +8,119 @@ const router = express.Router();
 // Database
 const db = require("../core/db");
 
-const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const urlencodedParser = express.urlencoded({ extended: true });
 
 // Daily
 schedule.scheduleJob("@daily", async () => {
-	try {
-		await db.poolPromise
-			.request()
-			.query("UPDATE DNMembership.dbo.Accounts SET claimDaily = 0");
-		console.log("Daily has been resetted");
-	} catch (err) {
-		console.log(`Unexpected error : ${err}`);
-	}
+  try {
+    await db.poolPromise
+      .request()
+      .query("UPDATE DNMembership.dbo.Accounts SET claimDaily = 0");
+    console.log("Daily has been resetted");
+  } catch (err) {
+    console.log(`Unexpected error : ${err}`);
+  }
 });
 
 router.get("/dashboard", async (req, res) => {
-	let user = req.session.user;
-	if (user) {
-		// Refresh session when reload
-		try {
-			let login = await db.poolPromise
-				.request()
-				.input("id", sql.NVarChar(50), user.AccountName)
-				.query(
-					"SELECT AccountID, AccountName, LastLoginDate, LastLogoutDate, RegisterDate, mail, cash, claimDaily FROM DNMembership.dbo.Accounts WHERE AccountName = @id "
-				);
+  let user = req.session.user;
+  if (user) {
+    // Refresh session when reload
+    try {
+      let login = await db.poolPromise
+        .request()
+        .input("id", sql.NVarChar(50), user.AccountName)
+        .query(
+          "SELECT AccountID, AccountName, LastLoginDate, LastLogoutDate, RegisterDate, mail, cash, claimDaily FROM DNMembership.dbo.Accounts WHERE AccountName = @id "
+        );
 
-			let getCharList = await db.poolPromise
-				.request()
-				.input("intAccountID", sql.Int, user.AccountID)
-				.execute("DNMembership.dbo.checkCashPoint");
-			let chars = getCharList.recordset;
+      let getCharList = await db.poolPromise
+        .request()
+        .input("intAccountID", sql.Int, user.AccountID)
+        .execute("DNMembership.dbo.checkCashPoint");
+      let chars = getCharList.recordset;
 
-			req.session.user = login.recordset[0];
-			user = req.session.user;
-			let gotCash = req.session.cash;
-			req.session.cash = null;
+      req.session.user = login.recordset[0];
+      user = req.session.user;
+      let gotCash = req.session.cash;
+      req.session.cash = null;
 
-			res.status(200).render("dashboard", {
-				opp: req.session.opp,
-				user,
-				chars,
-				gotCash,
-			});
-		} catch (err) {
-			console.log(`Unexpected error : ${err}`);
-		}
-	} else {
-		res.status(403).render("error403");
-	}
+      res.status(200).render("dashboard", {
+        opp: req.session.opp,
+        user,
+        chars,
+        gotCash,
+      });
+    } catch (err) {
+      console.log(`Unexpected error : ${err}`);
+    }
+  } else {
+    res.status(403).render("error403");
+  }
 });
 
 router.post("/dashboard/api/cash", async (req, res) => {
-	let user = req.session.user;
-	if (user) {
-		// Random amount cash 0-10000
-		let randomAmountCash = Math.floor(Math.random() * 10000);
-		if (user.claimDaily === 1) {
-			res.status(403).redirect("/");
-		} else {
-			try {
-				await db.poolPromise
-					.request()
-					.input("id", sql.NVarChar(50), user.AccountName)
-					.input("randomAmountCash", sql.Int, randomAmountCash)
-					.query(
-						"UPDATE DNMembership.dbo.Accounts SET cash = cash + @randomAmountCash, claimDaily = 1 WHERE AccountName = @id"
-					);
+  let user = req.session.user;
+  if (user) {
+    // Random amount cash 0-10000
+    let randomAmountCash = Math.floor(Math.random() * 10000);
+    if (user.claimDaily === 1) {
+      res.status(403).redirect("/");
+    } else {
+      try {
+        await db.poolPromise
+          .request()
+          .input("id", sql.NVarChar(50), user.AccountName)
+          .input("randomAmountCash", sql.Int, randomAmountCash)
+          .query(
+            "UPDATE DNMembership.dbo.Accounts SET cash = cash + @randomAmountCash, claimDaily = 1 WHERE AccountName = @id"
+          );
 
-				let login = await db.poolPromise
-					.request()
-					.input("id", sql.NVarChar(50), user.AccountName)
-					.query(
-						"SELECT AccountID, AccountName, LastLoginDate, LastLogoutDate, RegisterDate, mail, cash, claimDaily FROM DNMembership.dbo.Accounts WHERE AccountName = @id "
-					);
+        let login = await db.poolPromise
+          .request()
+          .input("id", sql.NVarChar(50), user.AccountName)
+          .query(
+            "SELECT AccountID, AccountName, LastLoginDate, LastLogoutDate, RegisterDate, mail, cash, claimDaily FROM DNMembership.dbo.Accounts WHERE AccountName = @id "
+          );
 
-				req.session.user = login.recordset[0];
-				user = req.session.user;
-				req.session.cash = randomAmountCash;
-				res.redirect("/dashboard");
-			} catch (err) {
-				console.log(`Unexpected error : ${err}`);
-			}
-		}
-	} else {
-		res.status(403).render("error403");
-	}
+        req.session.user = login.recordset[0];
+        user = req.session.user;
+        req.session.cash = randomAmountCash;
+        res.redirect("/dashboard");
+      } catch (err) {
+        console.log(`Unexpected error : ${err}`);
+      }
+    }
+  } else {
+    res.status(403).render("error403");
+  }
 });
 
 router.post("/dashboard/api/ftg", urlencodedParser, async (req, res) => {
-	let user = req.session.user;
-	if (user) {
-		try {
-			await db.poolPromise
-				.request()
-				.input("CharacterID", sql.BigInt, parseInt(req.body.CharacterID))
-				.query(
-					"UPDATE DNWorld.dbo.CharacterStatus SET Fatigue = Fatigue+5000 WHERE CharacterID = @CharacterID"
-				);
+  let user = req.session.user;
+  if (user) {
+    try {
+      await db.poolPromise
+        .request()
+        .input("CharacterID", sql.BigInt, parseInt(req.body.CharacterID))
+        .query(
+          "UPDATE DNWorld.dbo.CharacterStatus SET Fatigue = Fatigue+5000 WHERE CharacterID = @CharacterID"
+        );
 
-			await db.poolPromise
-				.request()
-				.input("CharacterID", sql.BigInt, parseInt(req.body.CharacterID))
-				.query(
-					"UPDATE DNWorld.dbo.Points SET Point = Point-1000 WHERE CharacterID = @CharacterID AND PointCode = 19"
-				);
+      await db.poolPromise
+        .request()
+        .input("CharacterID", sql.BigInt, parseInt(req.body.CharacterID))
+        .query(
+          "UPDATE DNWorld.dbo.Points SET Point = Point-1000 WHERE CharacterID = @CharacterID AND PointCode = 19"
+        );
 
-			res.redirect("/dashboard");
-		} catch (err) {
-			console.log(`Unexpected error : ${err}`);
-		}
-	} else {
-		res.status(403).render("error403");
-	}
+      res.redirect("/dashboard");
+    } catch (err) {
+      console.log(`Unexpected error : ${err}`);
+    }
+  } else {
+    res.status(403).render("error403");
+  }
 });
 
 module.exports = router;
--- src/routes/routesRegister.ts
+++ src/routes/routesRegister.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { check, validationResult } from "express-validator";
-import bodyParser from "body-parser";
 
 const router = express.Router();
 
@@ -8,155 +7,151 @@ const router = express.Router();
 const sql = require("mssql");
 const db = require("../core/db");
 
-const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const urlencodedParser = express.urlencoded({ extended: true });
 
 router.get("/register", (req, res) => {
-	let user = req.session.user;
-	if (user) {
-		res.status(200).redirect("/dashboard");
-		return;
-	}
+  let user = req.session.user;
+  if (user) {
+    res.status(200).redirect("/dashboard");
+    return;
+  }
 
-	res.status(200).render("register");
+  res.status(200).render("register");
 });
 
 // Register Validation
 router.post(
-	"/register",
-	urlencodedParser,
-	[
-		// ID Validation
-		check("id")
-			.isLength({ min: 6, max: 12 })
-			.withMessage("Username must be 6-12 chars")
-			.isAlphanumeric()
-			.withMessage("Username must not contain special chars")
-			.not()
-			.isEmpty()
-			.withMessage("Username cannot be empty")
-			.custom(async (mentionName: string) => {
-				if (!mentionName.match(/^[0-9a-zA-Z]+$/)) {
-					throw new Error("Username must not contain special chars");
-				}
-				const value = await isMentionNameInUse(mentionName);
-				if (value) {
-					throw new Error("Username already exists");
-				}
-			}),
-		// Email validation
-		check("email")
-			.isEmail()
-			.withMessage("Email is not valid")
-			.not()
-			.isEmpty()
-			.withMessage("Email cannot be empty")
-			.custom(async (mentionEmail: string) => {
-				const value = await isMentionEmailInUse(mentionEmail);
-				if (value) {
-					throw new Error("Email already exists");
-				}
-			}),
-		// Password Validation
-		check("password")
-			.isLength({ min: 6, max: 14 })
-			.withMessage("Password must be 6-14 chars")
-			.custom((value, { req }) => value === req.body.repeatPassword)
-			.withMessage("Passwords do not match")
-			.not()
-			.isEmpty()
-			.withMessage("Password cannot be empty"),
-	],
-	async (req: any, res: any) => {
-		const errors = validationResult(req);
-		// If Error IS NOT Empty
-		if (!errors.isEmpty()) {
-			const alert = errors.array();
-			for (let i in alert) {
-				if (alert[i].param === "id") {
-					var idError = alert[i].msg;
-				} else if (alert[i].param === "password") {
-					var passwordError = alert[i].msg;
-				} else if (alert[i].param === "email") {
-					var emailError = alert[i].msg;
-				}
-			}
-			res.status(400).render("register", {
-				idError,
-				emailError,
-				passwordError,
-				id: req.body.id,
-				email: req.body.email,
-			});
-		} else {
-			try {
-				await db.poolPromise
-					.request()
-					.input("id", sql.NVarChar(50), req.body.id)
-					.input("password", sql.VarChar(12), req.body.password)
-					.input("email", sql.VarChar(50), req.body.email)
-					.query(
-						"INSERT INTO DNMembership.dbo.Accounts (AccountName, AccountLevelCode, CharacterCreateLimit, CharacterMaxCount, RegisterDate, PublisherCode, Passphrase, mail) VALUES (@id, 0, 4, 8, GETDATE(), 0, CONVERT(BINARY(20),HashBytes('MD5',@password),2), @email)"
-					);
+  "/register",
+  urlencodedParser,
+  [
+    // ID Validation
+    check("id")
+      .isLength({ min: 6, max: 12 })
+      .withMessage("Username must be 6-12 chars")
+      .isAlphanumeric()
+      .withMessage("Username must not contain special chars")
+      .not()
+      .isEmpty()
+      .withMessage("Username cannot be empty")
+      .custom(async (mentionName: string) => {
+        if (!mentionName.match(/^[0-9a-zA-Z]+$/)) {
+          throw new Error("Username must not contain special chars");
+        }
+        const value = await isMentionNameInUse(mentionName);
+        if (value) {
+          throw new Error("Username already exists");
+        }
+      }),
+    // Email validation
+    check("email")
+      .isEmail()
+      .withMessage("Email is not valid")
+      .not()
+      .isEmpty()
+      .withMessage("Email cannot be empty")
+      .custom(async (mentionEmail: string) => {
+        const value = await isMentionEmailInUse(mentionEmail);
+        if (value) {
+          throw new Error("Email already exists");
+        }
+      }),
+    // Password Validation
+    check("password")
+      .isLength({ min: 6, max: 14 })
+      .withMessage("Password must be 6-14 chars")
+      .custom((value, { req }) => value === req.body.repeatPassword)
+      .withMessage("Passwords do not match")
+      .not()
+      .isEmpty()
+      .withMessage("Password cannot be empty"),
+  ],
+  async (req: any, res: any) => {
+    const errors = validationResult(req);
+    // If Error IS NOT Empty
+    if (!errors.isEmpty()) {
+      const alert = errors.array();
+      for (let i in alert) {
+        if (alert[i].param === "id") {
+          var idError = alert[i].msg;
+        } else if (alert[i].param === "password") {
+          var passwordError = alert[i].msg;
+        } else if (alert[i].param === "email") {
+          var emailError = alert[i].msg;
+        }
+      }
+      res.status(400).render("register", {
+        idError,
+        emailError,
+        passwordError,
+        id: req.body.id,
+        email: req.body.email,
+      });
+    } else {
+      try {
+        await db.poolPromise
+          .request()
+          .input("id", sql.NVarChar(50), req.body.id)
+          .input("password", sql.VarChar(12), req.body.password)
+          .input("email", sql.VarChar(50), req.body.email)
+          .query(
+            "INSERT INTO DNMembership.dbo.Accounts (AccountName, AccountLevelCode, CharacterCreateLimit, CharacterMaxCount, RegisterDate, PublisherCode, Passphrase, mail) VALUES (@id, 0, 4, 8, GETDATE(), 0, CONVERT(BINARY(20),HashBytes('MD5',@password),2), @email)"
+          );
 
-				req.session.message = "Registered succesfully";
-				res.redirect("/login");
-			} catch (err) {
-				console.log(`Unexpected error : ${err}`);
-			}
-		}
-	}
+        req.session.message = "Registered succesfully";
+        res.redirect("/login");
+      } catch (err) {
+        console.log(`Unexpected error : ${err}`);
+      }
+    }
+  }
 );
 
 function isMentionNameInUse(mentionName: String) {
-	return new Promise((resolve, reject) => {
-		sql.connect(db.config, (err: Error) => {
-			if (err) {
-				console.log(`Unexpected error : ${err}`);
-				return reject;
-			} else {
-				new sql.Request()
-					.input("id", sql.NVarChar(50), mentionName)
-					.query(
-						"SELECT COUNT(AccountName) AS ExistedAccountName FROM DNMembership.dbo.Accounts WHERE AccountName = @id",
-						(err: Error, result: { recordset: any }) => {
-							if (err) {
-								return reject;
-							} else {
-								return resolve(
-									result.recordset[0].ExistedAccountName > 0
-								);
-							}
-						}
-					);
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    sql.connect(db.config, (err: Error) => {
+      if (err) {
+        console.log(`Unexpected error : ${err}`);
+        return reject;
+      } else {
+        new sql.Request()
+          .input("id", sql.NVarChar(50), mentionName)
+          .query(
+            "SELECT COUNT(AccountName) AS ExistedAccountName FROM DNMembership.dbo.Accounts WHERE AccountName = @id",
+            (err: Error, result: { recordset: any }) => {
+              if (err) {
+                return reject;
+              } else {
+                return resolve(result.recordset[0].ExistedAccountName > 0);
+              }
+            }
+          );
+      }
+    });
+  });
 }
 
 function isMentionEmailInUse(mentionEmail: String) {
-	return new Promise((resolve, reject) => {
-		sql.connect(db.config, (err: Error) => {
-			if (err) {
-				console.log(`Unexpected error : ${err}`);
-				return reject;
-			} else {
-				new sql.Request()
-					.input("email", sql.VarChar(50), mentionEmail)
-					.query(
-						"SELECT COUNT(mail) AS ExistedEmail FROM DNMembership.dbo.Accounts WHERE mail = @email",
-						(err: Error, result: { recordset: any }) => {
-							if (err) {
-								return reject;
-							} else {
-								return resolve(
-									result.recordset[0].ExistedEmail > 0
-								);
-							}
-						}
-					);
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    sql.connect(db.config, (err: Error) => {
+      if (err) {
+        console.log(`Unexpected error : ${err}`);
+        return reject;
+      } else {
+        new sql.Request()
+          .input("email", sql.VarChar(50), mentionEmail)
+          .query(
+            "SELECT COUNT(mail) AS ExistedEmail FROM DNMembership.dbo.Accounts WHERE mail = @email",
+            (err: Error, result: { recordset: any }) => {
+              if (err) {
+                return reject;
+              } else {
+                return resolve(result.recordset[0].ExistedEmail > 0);
+              }
+            }
+          );
+      }
+    });
+  });
 }
 
 module.exports = router;
