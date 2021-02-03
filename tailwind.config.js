138d5db1b09f189b44f0fb1cf366a1e95607d9b4
ExLog <55097092+ExLog@users.noreply.github.com>
2021-02-03 22:29:58 +0800

    Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-	purge: {
-		enabled: true,
-		mode: 'all',
-		preserveHtmlElements: false,
-		content: ["./views/*.pug", "./views/include/*.pug"],
-	},
-	darkMode: false,
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: {
+    enabled: true,
+    mode: "all",
+    preserveHtmlElements: false,
+    content: ["./views/*.pug", "./views/include/*.pug"],
+  },
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
