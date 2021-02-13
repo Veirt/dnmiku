e93aaafed0b38f2e48fcbea5a20c55244506a839
ExLog <55097092+ExLog@users.noreply.github.com>
2021-02-13 20:59:32 +0800

    Use detect-port-alt and type declaration

--- src/routes/api/status.ts
+++ src/routes/api/status.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import detect from "detect-port";
+import detect from "detect-port-alt";
 
 const router = express.Router();
+// const detect = require("detect-port-alt");
 
 router.get("/api/status", async (_, res) => {
   const VillageStatus = await check_port(14400);
@@ -15,14 +16,14 @@ router.get("/api/status", async (_, res) => {
 
 async function check_port(port: number) {
   return detect(port)
-    .then((_port) => {
+    .then((_port: number) => {
       if (port == _port) {
         return "Offline";
       } else {
         return "Online";
       }
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       console.log(`Unexpected error : ${err}`);
       return "Error";
     });
