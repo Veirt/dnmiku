a3cdca5ed4f7607515f2a269bbfd7c36cf850100
ExLog <55097092+ExLog@users.noreply.github.com>
2021-03-20 23:02:47 +0800

    Migrate to Vue 3

--- query/AddClaimDailyQuery.sql
+++
@@ -1,7 +0,0 @@
--- DNMembership
-USE [DNMembership]
-GO 
-ALTER TABLE 
-  DNMembership.dbo.Accounts 
-ADD 
-  claimDaily int
--- src/routes/launcher.ts
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/launcher", (_: any, res: any) => {
-  res.status(200).render("launcher");
-});
-
-module.exports = router;
