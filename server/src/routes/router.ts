95469f5a0edd25f35a4b85374c88604f16e5f14d
ExLog <55097092+ExLog@users.noreply.github.com>
2021-03-27 18:37:18 +0800

    refactor: Make its own controller

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,6 +1,5 @@
 import { validateCreateAccount } from "../middlewares/validation.middleware";
-import { Account } from "../entity/Account";
-import { getConnection } from "typeorm";
+import {createAccount} from "../controllers/register";
 import express from "express";
 import cors from "cors";
 
@@ -13,28 +12,10 @@ router.use(express.urlencoded({ extended: true }));
 
 // GET
 router.get("/", (_, res) => res.json({ message: "pong" }));
-router.get("/api/v1/accounts", (req, res) => {});
+router.get("/api/v1/accounts/:id", async (req, res) => {
+});
 
 // POST
-router.post("/api/v1/accounts", validateCreateAccount, async (req, res) => {
-  const accountRepository = getConnection().getRepository(Account);
-
-  const account = new Account();
-  account.AccountName = req.body.AccountName;
-  account.Email = req.body.Email;
-  account.AccountLevelCode = 0;
-  account.RLKTPassword = req.body.Password;
-  account.LastLoginDate = null;
-  account.SecondAuthFailCount = 0;
-  account.SecondAuthCode = 1;
-  account.SecondAuthLockFlag = false;
-  account.CharacterCreateLimit = 4;
-  account.CharacterMaxCount = 8;
-  account.PublisherCode = 0;
-  account.RegisterDate = new Date();
-
-  await accountRepository.save(account);
-  res.status(201).json({ code: 201, message: "Successfully created account" });
-});
+router.post("/api/v1/accounts", validateCreateAccount, createAccount );
 
 module.exports = router;
