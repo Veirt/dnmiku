bca0ceb15e4a907ec734d9eb484852dd8d71dd90
ExLog <55097092+ExLog@users.noreply.github.com>
2021-04-04 14:31:16 +0800

    feat: Add vuex

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import store from "./store";
 import router from "./router";
-import 'virtual:windi.css'
+import "virtual:windi.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
---
+++ client/src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    apiUrl: import.meta.env.VITE_APP_API_ENDPOINT,
+  },
+  mutations: {},
+  actions: {},
+  modules: {},
+  getters: {},
+});
