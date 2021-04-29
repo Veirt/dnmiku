27181663521207e500bab4f5f097575d3efed1de
ExLog <55097092+ExLog@users.noreply.github.com>
2021-04-29 22:31:07 +0800

    fix: verify token on beforeEach instead of App.vue

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,36 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import store from "../store";
+import axios from "axios";
+
+const verifyToken = async () => {
+  const cookies = Object.fromEntries(
+    document.cookie.split(/; */).map((c) => {
+      const [key, v] = c.split("=", 2);
+      return [key, decodeURIComponent(v)];
+    })
+  );
+
+  try {
+    const res = await axios({
+      method: "GET",
+      baseURL: store.getters.apiUrl,
+      url: "/api/v1/accounts/@me",
+      headers: {
+        authorization: `Bearer ${cookies.token}`,
+      },
+      withCredentials: true,
+    });
+
+    if (res.data.accessToken) {
+      store.commit("setAccessToken", res.data.accessToken);
+      if (res.data.AccountLevelCode === 99) {
+        store.commit("setAdmin", true);
+      }
+    } else {
+      throw new Error("There is no token");
+    }
+  } catch (err) {}
+};
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -55,6 +87,16 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+let done = false;
+
+router.beforeEach(async (_, __, next) => {
+  if (!done) {
+    done = true;
+    await verifyToken();
+  }
+  next();
 });
 
 export default router;
