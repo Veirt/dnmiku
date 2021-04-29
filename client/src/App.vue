8a1773c4ea254c8658f5bd1880449ca27aaebb63
ExLog <55097092+ExLog@users.noreply.github.com>
2021-04-29 22:31:07 +0800

    fix: verify token on beforeEach instead of App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,46 +6,10 @@
 <script lang="ts">
 import NavBar from "./components/NavBar.vue";
 import { defineComponent } from "vue";
-import axios from "axios";
-import { useStore } from "vuex";
 
 export default defineComponent({
   name: "App",
   components: { NavBar },
-  setup() {
-    const store = useStore();
-
-    const verifyToken = async () => {
-      const cookies = Object.fromEntries(
-        document.cookie.split(/; */).map((c) => {
-          const [key, v] = c.split("=", 2);
-          return [key, decodeURIComponent(v)];
-        })
-      );
-
-      try {
-        const res = await axios({
-          method: "GET",
-          baseURL: store.getters.apiUrl,
-          url: "/api/v1/accounts/@me",
-          headers: {
-            authorization: `Bearer ${cookies.token}`,
-          },
-          withCredentials: true,
-        });
-
-        if (res.data.accessToken) {
-          store.commit("setAccessToken", res.data.accessToken);
-          if (res.data.AccountLevelCode === 99) {
-            store.commit("setAdmin", true);
-          }
-        } else {
-          throw new Error("There is no token");
-        }
-      } catch (err) {}
-    };
-
-    verifyToken();
-  },
+  setup() {},
 });
 </script>
