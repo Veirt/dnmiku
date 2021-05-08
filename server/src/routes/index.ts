f2eff53f0b119f14e4bb3301ea507c9dcaa24676
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-08 21:35:54 +0800

    style: Using import * as

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,23 +1,11 @@
 import routerConfig from "../config/router.config";
+import * as accountControllers from "../controllers/account.controller";
+import * as characterControllers from "../controllers/character.controller";
+import { createAccount, loginAccount } from "../controllers/auth.controller";
+import { getServerStatus } from "../controllers/server.controller";
 import { isAdmin, isAuthenticated } from "../middlewares/auth.middleware";
 import { validateCreateAccount } from "../middlewares/validation.middleware";
-import { loginAccount, createAccount } from "../controllers/auth.controller";
-import {
-  createAdminAccount,
-  deleteAccount,
-  editAccount,
-  getAccountById,
-  getAccountData,
-  getAccounts,
-} from "../controllers/account.controller";
 import express from "express";
-import { getServerStatus } from "../controllers/server.controller";
-import {
-  deleteCharacter,
-  editCharacter,
-  getCharacterById,
-  getCharacters,
-} from "../controllers/character.controller";
 
 const router = express.Router();
 routerConfig(router);
@@ -27,17 +15,62 @@ router.get("/status", getServerStatus);
 
 router.post("/auth", loginAccount);
 
-router.get("/accounts", isAuthenticated, isAdmin, getAccounts);
+router.get(
+  "/accounts",
+  isAuthenticated,
+  isAdmin,
+  accountControllers.getAccounts
+);
 router.post("/accounts", validateCreateAccount, createAccount);
-router.post("/accounts/admin", isAuthenticated, isAdmin, createAdminAccount);
-router.get("/accounts/@me", isAuthenticated, getAccountData);
-router.get("/accounts/:id", isAuthenticated, isAdmin, getAccountById);
-router.patch("/accounts/:id", isAuthenticated, isAdmin, editAccount);
-router.delete("/accounts/:id", isAuthenticated, isAdmin, deleteAccount);
+router.post(
+  "/accounts/admin",
+  isAuthenticated,
+  isAdmin,
+  accountControllers.createAdminAccount
+);
+router.get("/accounts/@me", isAuthenticated, accountControllers.getAccountData);
+router.get(
+  "/accounts/:id",
+  isAuthenticated,
+  isAdmin,
+  accountControllers.getAccountById
+);
+router.patch(
+  "/accounts/:id",
+  isAuthenticated,
+  isAdmin,
+  accountControllers.editAccount
+);
+router.delete(
+  "/accounts/:id",
+  isAuthenticated,
+  isAdmin,
+  accountControllers.deleteAccount
+);
 
-router.get("/characters", isAuthenticated, isAdmin, getCharacters);
-router.get("/characters/:id", isAuthenticated, isAdmin, getCharacterById);
-router.patch("/characters/:id", isAuthenticated, isAdmin, editCharacter);
-router.delete("/characters/:id", isAuthenticated, isAdmin, deleteCharacter);
+router.get(
+  "/characters",
+  isAuthenticated,
+  isAdmin,
+  characterControllers.getCharacters
+);
+router.get(
+  "/characters/:id",
+  isAuthenticated,
+  isAdmin,
+  characterControllers.getCharacterById
+);
+router.patch(
+  "/characters/:id",
+  isAuthenticated,
+  isAdmin,
+  characterControllers.editCharacter
+);
+router.delete(
+  "/characters/:id",
+  isAuthenticated,
+  isAdmin,
+  characterControllers.deleteCharacter
+);
 
 module.exports = router;
