4b0ef9b6fe99cf7bb9f25ee14ca6df8ed22067e9
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-10 10:13:53 +0800

    feat: Complete CharacterForm

--- client/src/views/Admin/Form/CharacterForm.vue
+++ client/src/views/Admin/Form/CharacterForm.vue
@@ -56,6 +56,85 @@
         />
       </div>
 
+      <div class="flex flex-row my-2">
+        <div class="flex flex-col w-1/2 mr-1">
+          <label class="block text-sm text-gray-600" for="LastVillageMapID"
+            >Last Village</label
+          >
+          <input
+            class="w-full px-3 py-1 text-gray-700 bg-gray-200 rounded outline-none"
+            id="LastVillageMapID"
+            v-model="character.CharacterStatus.LastVillageMapID"
+            type="number"
+            placeholder="Last Village ID"
+          />
+        </div>
+        <div class="flex flex-col w-1/2 ml-1">
+          <label class="block text-sm text-gray-600" for="MissionScore"
+            >Mission Score</label
+          >
+          <input
+            class="w-full px-3 py-1 text-gray-700 bg-gray-200 rounded outline-none"
+            id="MissionScore"
+            v-model="character.CharacterStatus.MissionScore"
+            type="number"
+            placeholder="Mission Score"
+          />
+        </div>
+      </div>
+
+      <div class="flex flex-row my-2">
+        <div class="flex flex-col w-1/2 mr-1">
+          <label class="block text-sm text-gray-600" for="Fatigue"
+            >Fatigue</label
+          >
+          <input
+            class="w-full px-3 py-1 text-gray-700 bg-gray-200 rounded outline-none"
+            id="Fatigue"
+            v-model="character.CharacterStatus.Fatigue"
+            type="number"
+            placeholder="Fatigue"
+          />
+        </div>
+        <div class="flex flex-col w-1/2 ml-1">
+          <label class="block text-sm text-gray-600" for="WeeklyFatigue"
+            >Weekly Fatigue</label
+          >
+          <input
+            class="w-full px-3 py-1 text-gray-700 bg-gray-200 rounded outline-none"
+            id="WeeklyFatigue"
+            v-model="character.CharacterStatus.WeeklyFatigue"
+            type="number"
+            placeholder="Weekly Fatigue"
+          />
+        </div>
+      </div>
+
+      <div class="flex flex-row my-2">
+        <div class="flex flex-col w-1/2 mr-1">
+          <label class="block text-sm text-gray-600" for="Coin">Gold</label>
+          <input
+            class="w-full px-3 py-1 text-gray-700 bg-gray-200 rounded outline-none"
+            id="Coin"
+            v-model="character.CharacterStatus.Coin"
+            type="number"
+            placeholder="Gold"
+          />
+        </div>
+        <div class="flex flex-col w-1/2 ml-1">
+          <label class="block text-sm text-gray-600" for="WarehouseCoin"
+            >Warehouse Gold</label
+          >
+          <input
+            class="w-full px-3 py-1 text-gray-700 bg-gray-200 rounded outline-none"
+            id="WarehouseCoin"
+            v-model="character.CharacterStatus.WarehouseCoin"
+            type="number"
+            placeholder="Warehouse Gold"
+          />
+        </div>
+      </div>
+
       <div class="mt-4">
         <button
           class="px-4 py-1 tracking-wider text-white transition delay-100 bg-gray-900 rounded hover:text-red-300"
