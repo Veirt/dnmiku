1d0f071d72cfa7de3d35afd10c4381c959fb2a56
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-13 12:57:38 +0800

    chore: remove endofline lint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
 	plugins: ["@typescript-eslint"],
 	rules: {
 		indent: ["error", "tab"],
-		"linebreak-style": ["error", "windows"],
 		quotes: ["error", "double"],
 		semi: ["error", "never"],
 	},
