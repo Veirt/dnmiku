1db6f3a6414e3ce202f6951b22a37ed003ed5031
Muhammad Dony Mulya <vveirt@gmail.com>
2021-11-18 18:43:24 +0800

    refactor(vite): remove vite compression

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 import compress from "vite-plugin-compress";
-import viteCompression from "vite-plugin-compression";
 import ViteComponents from "vite-plugin-components";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS(), compress(), viteCompression(), ViteComponents()],
+  plugins: [vue(), WindiCSS(), compress(), ViteComponents()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "/src"),
-    },
+      "@": path.resolve(__dirname, "/src")
+    }
   },
-  server: { fsServe: { root: ".." } },
+  server: { fsServe: { root: ".." } }
 });
